9caea5fd904da4cd7270f6dac9c56e5f68652250
forrune <esoifeoma1@gmail.com>
2025-06-30 10:08:04 +0100

    Update Routes

--- routes/bettingRoutes.js
+++ routes/bettingRoutes.js
@@ -1,14 +1,16 @@
+
 const express = require('express');
 const router = express.Router();
 
 router.post('/startbet', /* controller function here */);
-router.post('/endbet', /* controller function here */);
+router.post('/endbet',);
 
-router.get('/', /* controller function here */); // Get all bets
+router.get('/',); //get all bets
 router.get('/getbet/:betId', /* controller function here */);
 
 router.put('/updatebet/:betId', /* controller function here */);
 
 router.delete('/:telegramId', /* controller function here */);
 
-module.exports = router;
+const bettingRouter = router;
+module.exports = bettingRouter;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,53 +1,24 @@
+
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
 
-// Register a new user
-router.post('/register', userController.registerUser);
+router.post('/register', /* controller function here */);
+router.post('/deposit/:telegramId', );
+router.post('/placebet/:telegramId',);
+router.post('/stakenft/:telegramId',);
+router.post('/invite/:telegramId',);
+router.post('/withdraw/:telegramId',);
 
-// Deposit TON or credits
-router.post('/deposit/:telegramId', userController.depositTo);
 
-// Place a bet
-router.post('/placebet/:telegramId', userController.placeBet);
+router.get('/getuser/:telegramId', /* controller function here */);
+router.get('/getbetsbyuser/:telegramId',);
+router.get('/getstakednfts/:telegramId',);
 
-// Stake NFT (stub - create this function if needed)
-router.post('/stakenft/:telegramId', (req, res) => {
-  res.send("Stake NFT - to be implemented");
-});
+router.put('/:telegramId', /* controller function here */);
 
-// Invite friend (stub - create this function if needed)
-router.post('/invite/:telegramId', (req, res) => {
-  res.send("Invite Friend - to be implemented");
-});
+router.delete('/:telegramId', /* controller function here */);
 
-// Withdraw funds
-router.post('/withdraw/:telegramId', userController.withdraw);
+router.get('/', /* controller function here */);
 
-// Get a single user
-router.get('/getuser/:telegramId', userController.getUser);
-
-// Get user's bets
-router.get('/getbetsbyuser/:telegramId', userController.getBetsByUser);
-
-// Get staked NFTs (stub - needs to be implemented)
-router.get('/getstakednfts/:telegramId', (req, res) => {
-  res.send("Get staked NFTs - to be implemented");
-});
-
-// Update user info (generic PUT)
-router.put('/:telegramId', (req, res) => {
-  res.send("Update user - to be implemented");
-});
-
-// Delete a user
-router.delete('/:telegramId', (req, res) => {
-  res.send("Delete user - to be implemented");
-});
-
-// Get all users
-router.get('/', (req, res) => {
-  res.send("Get all users - to be implemented");
-});
-
-module.exports = router;
+const userRouter = router;
+module.exports = userRouter;
